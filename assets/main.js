239e8b06a18a8a92ed19cf2c07e93de31c16a7e2
MerisStupar <merisstupar11@gmail.com>
2021-11-05 14:31:05 +0100

    A function is added for the box when click to play music and it will also show title

--- assets/main.js
+++ assets/main.js
@@ -2,9 +2,25 @@ let closePlayer = document.querySelector('#close-player');
 let musicPlayer = document.querySelector(".music-player");
 let boxContainer = document.querySelector(".container .box-container");
 
-
+// Closing player on click 
 closePlayer.onclick = () =>{
     closePlayer.classList.toggle('fa-times');
     musicPlayer.classList.toggle('active');
     boxContainer.classList.toggle('active');
 }
+
+let boxes = document.querySelectorAll('.container .box-container .box');
+// On click for every box player will pop-up
+boxes.forEach(box=>{
+    box.onclick=()=>{
+        let src = box.getAttribute('data-src');
+        let text = box.querySelector('.content h3').innerText;
+        closePlayer.classList.add('fa-times');
+        musicPlayer.classList.add('active');
+        boxContainer.classList.add('active');
+        musicPlayer.querySelector('h3').innerText = text;
+        musicPlayer.querySelector('audio').src=src;
+        musicPlayer.querySelector('audio').play();
+
+    }
+});
